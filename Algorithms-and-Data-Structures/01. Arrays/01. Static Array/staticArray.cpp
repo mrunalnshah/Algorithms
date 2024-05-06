1e44ecacdd3e44d8a936130f4acbc6e167bcd94d
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2024-05-06 17:32:00 +0530

    staticArray added in Array
    staticArray added in Array

---
+++ Algorithms-and-Data-Structures/01. Arrays/01. Static Array/staticArray.cpp
@@ -0,0 +1,127 @@
+/*
+    Author  : Mrunal Nirajkumar Shah
+    Date    : 6th of May, 2024
+
+    Details : Static Array Class with Insert, Delete and Modify Methods
+*/
+
+#include <iostream>
+
+// Static Array Class with Methods
+template <class Type, int Num>
+class myStaticArray {
+  int top = -1;
+  Type myArray[Num];
+
+ public:
+  void print() {
+    for (int i = 0; i <= top; i++) {
+      std::cout << myArray[i] << std::endl;
+    }
+  }
+
+  // ...INSERT...
+  void myInsert(Type data) {
+    if (top < Num) {
+      top++;
+      myArray[top] = data;
+    } else {
+      std::cout << "top is " << top << " and Size is " << Num << std::endl;
+    }
+  }
+
+  void myInsert(Type data, int index) {
+    if (top < index && !(top == index - 1)) {
+      std::cout << "top is " << top << " and Size is " << Num
+                << " and Index is  " << index << std::endl;
+    } else {
+      if (top < Num) {
+        top++;
+        Type tempArray[Num];
+
+        for (int i = 0; i <= top; i++) {
+          if (i < index) {
+            tempArray[i] = myArray[i];
+          } else if (i == index) {
+            tempArray[i] = data;
+          } else {
+            tempArray[i] = myArray[i - 1];
+          }
+        }
+
+        for (int i = 0; i <= top; i++) {
+          myArray[i] = tempArray[i];
+        }
+      } else {
+        std::cout << "top is " << top << " and Size is " << Num << std::endl;
+      }
+    }
+  }
+
+  // ...DELETE...
+  void myDelete() {
+    if (top != -1) {
+      --top;
+    } else {
+      std::cout << "top is " << top << " and Size is " << Num << std::endl;
+    }
+  }
+
+  void myDelete(int index) {
+    if (top < index) {
+      std::cout << "top is " << top << " and Size is " << Num
+                << " and Index is  " << index << std::endl;
+    } else {
+      if (top != -1) {
+        Type tempArray[Num];
+        for (int i = 0; i <= top; i++) {
+          if (i < index) {
+            tempArray[i] = myArray[i];
+          } else {
+            tempArray[i] = myArray[i + 1];
+          }
+        }
+        top--;
+
+        for (int i = 0; i <= top; i++) {
+          myArray[i] = tempArray[i];
+        }
+      } else {
+        std::cout << "top is " << top << " and Size is " << Num << std::endl;
+      }
+    }
+  }
+
+  // ...MODIFY...
+  void myModify(Type data, int index) { myArray[index] = data; }
+};
+
+
+
+// MAIN FUNCTION 
+int main() {
+  const int num = 5;
+
+  myStaticArray<char, num> myArray;
+
+  std::cout << " INSERT " << std::endl;
+  myArray.myInsert('a', 0);
+  myArray.myInsert('b', 1);
+  myArray.myInsert('c', 2);
+  myArray.myInsert('d', 3);
+  myArray.myInsert('e', 4);
+
+  myArray.print();
+
+  std::cout << " DELETE " << std::endl;
+  myArray.myDelete();
+  myArray.myDelete(2);
+
+  myArray.print();
+
+  std::cout << " MODIFY " << std::endl;
+  myArray.myModify('x', 0);
+  myArray.print();
+
+  return 0;
+}
