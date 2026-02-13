02bbd85f305bebf378745a85747c2f39342e1ace
Mrunal Shah <mrunalnshah@protonmail.com>
2026-02-13 10:59:07 +0530

    DSA ADDON

---
+++ CodingArena/LeetCode/0020_Valid-Parentheses/Python/01.py
@@ -0,0 +1,57 @@
+"""
+Author: Mrunal Nirajkumar Shah
+Date  : 13th February, 2026
+
+LeetCode: 20. Valid Parentheses
+
+Solution Description:
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
+        stack = []
+
+        for char in s:
+            if char not in ["(", "{", "[", ")", "}", "]"]:
+                continue
+            else:
+                if char in ["(", "{", "["]:
+                    stack.append(char)
+                else:
+                    if len(stack) == 0:
+                        return False
+                    
+                    if (char == ")" and stack.pop() == "(") or (char == "}" and stack.pop() == "{") or (char == "]" and stack.pop() == "["):
+                        continue
+                    else:
+                        return False
+
+        if len(stack) != 0:
+            return False
+        
+        return True
+
+def main():
+    list_s = ["()",
+              "()[]{}",
+              "(]",
+              "([])",
+              "([)]",
+              "["
+              ]
+    
+    solve = Solution()
+
+    result = []
+    for s in list_s:
+        result.append(solve.isValid(s))
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
