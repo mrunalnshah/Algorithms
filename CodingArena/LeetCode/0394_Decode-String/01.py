0d1a23f73ff373235a6bf279dd5d1a7b11c9d305
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2026-06-20 12:26:33 +0530

    DSA - CODING ARENA - LEETCODE

--- CodingArena/LeetCode/0394_Decode-String/01.py
+++ CodingArena/LeetCode/0394_Decode-String/01.py
@@ -3,15 +3,47 @@ Problem: 394. Decode String
 Link: https://leetcode.com/problems/decode-string/
 
 Author: Mrunal Nirajkumar Shah
-Date: X June, 2026
+Date: 20 June, 2026
 
-Time Complexity: 
-Space Complexity:
+Time Complexity: Depends on Input
+Space Complexity: O(n)
+
+where n is the size of string input.
 """
 
 class Solution:
+    """
+    Input: "3[a]2[bc]"
+    Output: aaabcbc
+    """
     def decodeString(self, s: str) -> str:
-        ...
+        """
+        append all char to stack until "]". once end brackets are detected,
+        append the substring * k to the stack.
+        """
+        s_stack = []
+
+        for i in range(len(s)):
+            if s[i] != "]":
+                s_stack.append(s[i])
+            else:
+                substring = ""
+
+                while s_stack[-1] != "[":
+                    substring = s_stack.pop() + substring
+                
+                s_stack.pop()
+
+                k = ""
+                while s_stack and s_stack[-1].isdigit():
+                    k = s_stack.pop() + k
+
+                k = int(k)
+
+                s_stack.append(k * substring)
+        
+        return "".join(s_stack)
+
 
 def main():
     list_s = [
